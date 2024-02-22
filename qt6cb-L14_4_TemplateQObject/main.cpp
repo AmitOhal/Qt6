4f85f23d2b369b4ed22263c81e462adaf8a6ad4e
Amit Ashok Ohal <amit@Amits-MacBook-Pro.local>
2024-02-22 23:05:19 +0530

    Template and QObject

---
+++ qt6cb-L14_4_TemplateQObject/main.cpp
@@ -0,0 +1,23 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+template<class T>
+void display(T value)
+{
+    qInfo()<<value;
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    display<int>(1);
+    display<double>(3.14);
+    display<QString>("Hello");
+
+    QObject obj;
+
+    display<QObject *>(&obj);
+
+    return a.exec();
+}
